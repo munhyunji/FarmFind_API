b1082289b5c6ba8d9e10697827d8785973ca1bcd
문현지 <hjmoon@wemaginesoft.co.kr>
2023-12-26 13:23:22 +0900

    View 도메인 CORS 허용 추가

--- farmfind_api/src/main/java/com/study/farmfind/FarmfindApiApplication.java
+++ farmfind_api/src/main/java/com/study/farmfind/FarmfindApiApplication.java
@@ -26,6 +26,8 @@ public class FarmfindApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:8070");
+                registry.addMapping("/**").allowedOrigins("http://3.35.93.254:8080");
+                registry.addMapping("/**").allowedOrigins("https://3.35.93.254:8080");
             }
         };
     }
